e572eb48285fa1c8f316a20ef491c9c31cdadbbe
Plamen Petrov <biznetmen@gmail.com>
2023-08-21 11:45:27 +0300

    feat: add error page

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
+import React from "react";
 // import App from './App.jsx'
-import Root from "./routes/root"
-import './index.css'
-import * as ReactDOM from "react-dom/client"
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom"
+import Root from "./routes/root";
+import "./index.css";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./error-page";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
-])
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
