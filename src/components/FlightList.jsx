f28e55e0f7a2ba4112738f57bd224761d8093821
Plamen Petrov <biznetmen@gmail.com>
2023-08-21 11:17:00 +0300

    feat: install react-router-dom, add Link

--- src/components/FlightList.jsx
+++ src/components/FlightList.jsx
@@ -1,6 +1,7 @@
 import "./FlightList.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Link from "react-router-dom";
 
 function FlightList() {
   const [flights, setFlights] = useState([]);
@@ -19,22 +20,22 @@ function FlightList() {
       {flights.map((flight) => (
         <li key={flight.flight_number} className="card">
           <div className="card-header">
-          <img src={flight.links.mission_patch_small} alt={flight.mission_name}/>
+            <img
+              src={flight.links.mission_patch_small}
+              alt={flight.mission_name}
+            />
             <h2>{flight.mission_name}</h2>
-            </div>
+          </div>
           <div className="card-body">
             <p>Flight number: {flight.flight_number}</p>
-          <p>Launch year: {flight.launch_year}</p>
-          <p>Launch date: {flight.launch_date_local}</p>
-          <p>Site: {flight.launch_site.site_name_long}</p>
-          {/* <p>Details: {flight.details}</p> */}
-          <p>Crew: {flight.crew}</p>
+            <p>Launch year: {flight.launch_year}</p>
+            <p>Launch date: {flight.launch_date_local}</p>
+            <p>Site: {flight.launch_site.site_name_long}</p>
           </div>
           <div className="card-footer">
             <button className="btn">Details</button>
-            <button className="btn btn-outline">More...</button>
+            <Link to={flight.links.article_link}><button className="btn btn-outline">More...</button></Link>
           </div>
-          
         </li>
       ))}
     </ul>
